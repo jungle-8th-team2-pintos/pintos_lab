6b490f4d060eb156551fbfe34c57f265539e575f
badapodo <ksg6736@gmail.com>
2025-05-21 04:19:55 +0900

    feat: args

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,15 +1,26 @@
 #include "userprog/syscall.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+#include "intrinsic.h"
+#include "threads/flags.h"
+#include "threads/interrupt.h"
+#include "threads/loader.h"
+#include "threads/palloc.h"
+#include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/gdt.h"
+#include "userprog/process.h"
+#include "vm/vm.h"
+#include <list.h>
 #include <stdio.h>
 #include <syscall-nr.h>
-#include "threads/interrupt.h"
-#include "threads/thread.h"
-#include "threads/loader.h"
-#include "userprog/gdt.h"
-#include "threads/flags.h"
-#include "intrinsic.h"
 
-void syscall_entry (void);
-void syscall_handler (struct intr_frame *);
+#include "filesys/directory.h"
+#include "filesys/fat.h"
+#include "filesys/inode.h"
+
+void syscall_entry(void);
+void syscall_handler(struct intr_frame *);
 
 /* System call.
  *
@@ -24,23 +35,53 @@ void syscall_handler (struct intr_frame *);
 #define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
-void
-syscall_init (void) {
-	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
-			((uint64_t)SEL_KCSEG) << 32);
-	write_msr(MSR_LSTAR, (uint64_t) syscall_entry);
+void syscall_init(void) {
+    write_msr(MSR_STAR,
+              ((uint64_t)SEL_UCSEG - 0x10) << 48 | ((uint64_t)SEL_KCSEG) << 32);
+    write_msr(MSR_LSTAR, (uint64_t)syscall_entry);
 
-	/* The interrupt service rountine should not serve any interrupts
-	 * until the syscall_entry swaps the userland stack to the kernel
-	 * mode stack. Therefore, we masked the FLAG_FL. */
-	write_msr(MSR_SYSCALL_MASK,
-			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+    /* The interrupt service rountine should not serve any interrupts
+     * until the syscall_entry swaps the userland stack to the kernel
+     * mode stack. Therefore, we masked the FLAG_FL. */
+    write_msr(MSR_SYSCALL_MASK,
+              FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
 
-/* The main system call interface */
-void
-syscall_handler (struct intr_frame *f UNUSED) {
-	// TODO: Your implementation goes here.
-	printf ("system call!\n");
-	thread_exit ();
+void syscall_handler(struct intr_frame *f UNUSED) {
+
+    // printf("[DEBUG] syscall_handler invoked! rax=%lld\n", f->R.rax);
+
+    uint64_t syscall_num = f->R.rax;
+
+    switch (syscall_num) {
+    case SYS_WRITE:
+        f->R.rax = write(f->R.rdi, f->R.rsi, f->R.rdx);
+        break;
+
+    case SYS_EXIT:
+        exit(f->R.rdi);
+        break;
+
+    default:
+        printf("Unknown syscall number: %lld\n", syscall_num);
+        thread_exit();
+    }
+}
+
+// todo: This is temporary measure for test
+int write(int fd, const void *buffer, unsigned size) {
+    const char *buf = buffer;
+
+    if (fd == 1) {
+        for (unsigned i = 0; i < size; i++) {
+            putchar(buf[i]);
+        }
+        return size;
+    }
+    return -1;
+}
+
+void exit(int status) {
+    printf("%s: exit(%d)\n", thread_name(), status);
+    thread_exit();
 }
