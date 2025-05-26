0c88263fba392bfc8bdbee118b0fb32e28e79755
badapodo <ksg6736@gmail.com>
2025-05-26 14:33:25 +0900

    refactor: 모듈 분리 없이 process.c에서 arg 파싱 진행

--- include/lib/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LIB_UTIL_H
-#define __LIB_UTIL_H
-
-#include "include/userprog/process.h"
-
-char *extract_program_name(const char *cmdline);
-int parse_arguments(char *file_name, char **argv);
-
-#endif /* lib/util.h */
